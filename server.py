4630f78d5530cbaedc6f1a786ca9de67a969c840
Dusernajder <dusungulu.tibi@gmail.com>
2020-03-12 18:04:31 +0100

    server.py somewhat setup

--- server.py
+++ server.py
@@ -4,13 +4,26 @@ import data_handler
 
 app = Flask(__name__)
 
+data = []
+
 
 @app.route('/')
+def home():
+    data = data_handler.get_elements_csv('data.csv')
+    return render_template('home.html', titles=data_handler.DATA_HEADER, elements=data)
+
+
+@app.route('/story', methods=['GET', 'POST'])
+def story():
+    return render_template('story.html')
+
+
 @app.route('/list')
 def route_list():
     user_stories = data_handler.get_all_user_story()
 
     return render_template('list.html', user_stories=user_stories)
+
 
 if __name__ == '__main__':
     app.run(
