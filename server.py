a52f066f7569481ed215c3441b8c77e045fd29f5
Dusernajder <dusungulu.tibi@gmail.com>
2020-03-14 12:41:18 +0100

    /story route done, added new variable: headers

--- server.py
+++ server.py
@@ -4,17 +4,32 @@ import data_handler
 
 app = Flask(__name__)
 
-data = []
+headers = [' '.join(header.split("_")) for header in data_handler.DATA_HEADER]
+data = {}
 
 
 @app.route('/')
 def home():
-    data = data_handler.get_elements_csv('data.csv')
-    return render_template('home.html', titles=data_handler.DATA_HEADER, elements=data)
+    data = data_handler.get_elements_csv()
+    return render_template('home.html', headers=headers, data=data[1:])
 
 
 @app.route('/story', methods=['GET', 'POST'])
 def story():
+    if request.method == 'POST':
+        title = request.form['title']
+        user_story = request.form['story']
+        acceptance_criteria = request.form['acceptance_criteria']
+        business_value = request.form['business_value'] + " point"
+        estimation = request.form['estimation'] + "h"
+        status = request.form['status']
+        _id = data_handler.get_id()
+        row = [_id, title, user_story, acceptance_criteria, business_value, estimation, status]
+
+        data_handler.add_element_csv(row)
+
+        return redirect('/')
+
     return render_template('story.html')
 
 
@@ -22,7 +37,7 @@ def story():
 def route_list():
     user_stories = data_handler.get_all_user_story()
 
-    return render_template('list.html', user_stories=user_stories)
+    return render_template('list.html', user_stories=user_stories, status='')
 
 
 if __name__ == '__main__':
