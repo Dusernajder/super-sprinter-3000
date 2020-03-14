e544308d523955e6c1004b131811de5bf1413e36
Dusernajder <dusungulu.tibi@gmail.com>
2020-03-14 12:36:45 +0100

    get_elements_csv now uses dictionary, add_element_csv cleanup, get_id added

--- data_handler.py
+++ data_handler.py
@@ -2,25 +2,25 @@ import csv
 import os
 
 DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'data.csv'
-DATA_HEADER = ['id', 'title', 'user story', 'acceptance criteria', 'business value', 'estimation', 'status']
+DATA_HEADER = ['id', 'title', 'user_story', 'acceptance_criteria', 'business_value', 'estimation', 'status']
 STATUSES = ['planning', 'todo', 'in progress', 'review', 'done']
 
 
-def get_all_user_story():
-    return []
-
-
-def get_elements_csv(file_csv):
+def get_elements_csv():
     temp_lst = []
-    with open(file_csv) as file:
-        for line in file:
-            temp_lst.append(line.split(";"))
+    with open(DATA_FILE_PATH) as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            dictionary = {key: value for key, value in zip(DATA_HEADER, row)}
+            temp_lst.append(dictionary)
         return temp_lst
 
 
-def add_element_csv(file, element):
-    temp = get_elements_csv(file)
-    temp.append(';'.join(element))
-    with open(file, "w")as file:
-        for row in temp:
-            file.write(row)
+def add_element_csv(row):
+    with open(DATA_FILE_PATH, 'a') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(row)
+
+
+def get_id():
+    return len(get_elements_csv())
